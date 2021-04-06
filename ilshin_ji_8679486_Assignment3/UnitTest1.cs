68ce4f2f60314fc8c783c5fdb03d0419cf98204f
ilshin ji <ilshin715@gmail.com>
2021-04-06 12:28:17 -0400

    project initialization

---
+++ ilshin_ji_8679486_Assignment3/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ilshin_ji_8679486_Assignment3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
